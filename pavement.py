c1ef80e3c0a961db777f9f56d0974ea66fbae042
Maciek Kazirod <maciej.kazirod@gmail.com>
2014-09-26 00:32:21 +0200

    0.6.5 release

--- pavement.py
+++ pavement.py
@@ -7,7 +7,7 @@ setup(
     name="pyage-forams",
     description="Forams package for Pyage platform",
     packages=find_packages(),
-    version="0.6.4",
+    version="0.6.5",
     author="Maciej Kaziród",
     author_email="kazirod.maciej@gmail.com",
     requires=['matplotlib', 'pyage']
